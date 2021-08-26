7ab7ddb60f6d3b4652c1a55c715cb097a428810d
tjreese90 <60903273+Tjreese@users.noreply.github.com>
2021-08-25 22:00:45 -0500

    adding index, script, and styling for quiz app

---
+++ script.js
@@ -0,0 +1,37 @@
+const quizData = [
+    {
+        question: 'How old am I?',
+
+        a: '10',
+        b: '17',
+        c: '21',
+        d: '32',
+        correct: 'c' 
+    },
+    {
+        question: 'When do I graduate?',
+        a: '2021',
+        b: '2034',
+        c: '2012',
+        d: '2022',
+        correct: 'd'
+    },
+    {
+        question: 'What is my favorite color?',
+        a: 'Green',
+        b: 'Blue',
+        c: 'Yellow',
+        d: 'Purple',
+        correct: 'b'
+    },
+    {
+        question: 'What is my favorite food?',
+        a: 'Orange Chicken',
+        b: 'Burben Chicken',
+        c: 'Crab Legs',
+        d: 'All the above',
+        correct: 'd'
+
+    }
+
+]
